5539ba60d671bcb0a79b10a6edac690481bf877b
Igor Rud <rud.igor23@gmail.com>
2019-02-07 00:04:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -29,17 +29,3 @@ languages = {
 
   }
 }
-
-def reformat_languages(languages)
-new_hash = {}
-languages.each do |style, lang|
-  languages[style].each do |language, type|
-    new_hash[language] = {}
-end
-end
-new_hash.each do |language, val|
-  language[type] = ""
-  language[style] = ""
-end
-new_hash
-end
