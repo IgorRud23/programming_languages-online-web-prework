e4e0aa0249f2b62ea70c4e618fe6d57e8a278f6d
Igor Rud <rud.igor23@gmail.com>
2019-02-07 00:11:14 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -29,3 +29,26 @@ languages = {
 
   }
 }
+
+def reformat_languages(language_hash)
+new_hash = {}
+language_hash.each do |language_one, languages|
+  languages.each do |language, attributes|
+    attributes.each do |attribute, att_value|
+      if language = :javascript
+        new_hash[language] = {
+          :type => att_value,
+          :style => [:oo, :functional]
+        }
+      else
+        new_hash[language] = {
+          :type => att_value,
+          :style => [language_one]
+        }
+end
+end
+end
+end
+return new_hash
+end
+  
