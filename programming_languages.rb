e06bd481a1d5dd0068f9f2934f80a4cd5e55fa64
Igor Rud <rud.igor23@gmail.com>
2019-02-07 00:03:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,3 @@
-def reformat_languages(languages)
-new_hash = {}
-languages.each do |style, lang|
-  languages[style].each do |language, type|
-    new_hash[language] = {}
-end
-end
-new_hash.each do |language, val|
-  language[type] = ""
-  language[style] = ""
-end
-new_hash
-end
-
 languages = {
   :oo => {
     :ruby => {
@@ -43,3 +29,19 @@ languages = {
 
   }
 }
+
+def reformat_languages(languages)
+new_hash = {}
+languages.each do |style, lang|
+  languages[style].each do |language, type|
+    new_hash[language] = {}
+end
+end
+new_hash.each do |language, val|
+  language[type] = ""
+  language[style] = ""
+end
+new_hash
+end
+
+reformat_languages(languages)
