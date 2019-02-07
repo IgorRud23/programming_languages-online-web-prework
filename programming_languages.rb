5bbe8d1626256c609ac5dddb92ff13e8b50cc3f2
Igor Rud <rud.igor23@gmail.com>
2019-02-07 00:19:30 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,13 +32,18 @@ languages = {
 
 def reformat_languages(language_hash)
   h = {}
-  languages.each do |k, v|
-      v.each do |k1, v1|
-          if h[k1]
-              h[k1][:style] << k
-          else
-              h[k1] = {type: v1[:type], style: [k]}
-          end
-      end
+languages.each do |k, v| # oo or func
+  puts "k=#{k}, v=#{v}"
+  v.each do |k1, v1| # ruby/python
+    puts "  k1=#{k1}, v1=#{v1}"
+    if h[k1]
+      puts "  h[#{k1}]=#{h[k1]} (truthy)"
+      h[k1][:style] << k
+      puts "  h after h[#{k1}][:style] << #{k}: #{h}"
+    else
+      puts "  h[#{k1}].nil?=true (falsy)"
+      h[k1] = {type: v1[:type], style: [k]}
+      puts "  h after h[#{k1}] = {type: v1[:type], style: #{k}}: #{h}"
+    end
   end
 end
